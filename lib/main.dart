0b8c05bfbcdfd7dad8cc287c4839cd26a15fe8cc
Theekshana <theenutheekshana@gmail.com>
2025-01-23 23:47:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:news_beeper/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      darkTheme: ThemeData.dark(
+        useMaterial3: true
+      ),
+      theme: ThemeData.light(
+        useMaterial3: true
+      ),
+      themeMode: ThemeMode.system,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+
